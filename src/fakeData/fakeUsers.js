1cf5d1b8c433547b4ec1c7274dc278888d071527
van_dark_fong <ctrl.logic.feng@gmail.com>
2026-05-13 14:53:49 +0700

    setup server.js

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,33 @@
+export const users = [
+  {
+    id: 1,
+    username: "John Doe",
+    email: "john.doe@example.com",
+    password: "password123"
+  },
+  {
+    id: 2,
+    username: "Jane Smith",
+    email: "jane.smith@example.com",
+    password: "password456"
+  },
+  {
+    id: 3,
+    username: "Alice Johnson",
+    email: "alice.johnson@example.com",
+    password: "password789"
+  },
+  {
+    id: 4,
+    username: "Alice Williams",
+    email: "alice.williams@example.com",
+    password: "password012"
+  },
+  {
+    id: 5,
+    username: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    password: "password345"
+  }
+];
+
